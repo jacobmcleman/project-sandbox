5d05cfa5bcc52749cb7ae5996383d071edf3975a
Jake McLeman <jake@mcleman.com>
2024-01-14 20:47:45 -0800

    fixed mouse/world space translation

--- src/sandsim.rs
+++ src/sandsim.rs
@@ -302,22 +302,7 @@ fn world_interact(
 
         // check if the cursor is inside the window and get its position
         if let Some(screen_pos) = wnd.cursor_position() {
-            // get the size of the window
-            let window_size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
-
-            // convert screen position [0..resolution] to ndc [-1..1] (gpu coordinates)
-            let ndc = (screen_pos / window_size) * 2.0 - Vec2::ONE;
-
-            // matrix for undoing the projection and camera transform
-            let ndc_to_world =
-                camera_transform.compute_matrix() * camera.projection_matrix().inverse();
-
-            // use it to convert ndc to world-space coordinates
-            let world_pos = ndc_to_world.project_point3(ndc.extend(-1.0));
-
-            // reduce it to a 2D value
-            let world_pos: Vec2 = world_pos.truncate();
-
+            let world_pos = camera.viewport_to_world_2d(camera_transform, screen_pos).unwrap();
             let gridpos = GridVec::new(world_pos.x as i32, world_pos.y as i32);
 
             if buttons.pressed(MouseButton::Left) {
